a6d44ae9b069ce5add3c1f049222c6378751231b
kengomasunari2025 <kengomasunari2025@gmail.com>
2025-01-27 11:38:25 +0900

    202501271138

---
+++ src/React/Kengo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SlickSlider from "./SlickSlider/SlickSlider";
+// import ContactForm from "./ContactForm/ContactForm";
+import Footer from "./Footer/Footer";
+
+const Kengo = () => {
+  return (
+    <div className="bg-white">
+      <SlickSlider />
+      {/* <ContactForm /> */}
+      <Footer />
+    </div>
+  );
+};
+export default Kengo;
